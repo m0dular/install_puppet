5867c9ea27969411f63462f92c27cdb04ef8b05f
Adrian Parreiras Horta <adrian.parreiras-horta@puppet.com>
2019-11-25 12:59:37 -0800

    WIP RHEL and Debian bugfixes.  SLES TODO

--- plans/provision_master.pp
+++ plans/provision_master.pp
@@ -6,19 +6,16 @@ plan install_puppet::provision_master(
 
   $master_facts = run_task('facts', $master)
   $os_family = $master_facts.first.value['os']['family']
-  $name = $master_facts.first.value['os']['name']
+  $master_name = $master_facts.first.value['os']['name']
+  $major_version = $master_facts.first.value['os']['release']['major']
 
   case $os_family {
     'debian': {
-      $gpg_result = run_task(
-        'install_puppet::install_gpg_key', $master, name => $os_family, gpg_url => $gpg_url
-      )
-
-      $codename = $master_facts.first.value['os']['codename']
+      $codename = $master_facts.first.value['os']['distro']['codename']
       $repo_url = "deb http://apt.puppetlabs.com $codename puppet"
     }
     'redhat', 'sles', 'suse', 'fedora': {
-      case $name {
+      case $master_name {
         'fedora': {
           #TODO
           $extra_args = '-f -g -n puppet'
@@ -35,17 +32,17 @@ plan install_puppet::provision_master(
           $repo_url = "http://yum.puppetlabs.com/puppet/puppet-release-${install_name}-${major_version}.noarch.rpm"
         }
       }
-      $gpg_result = run_task(
-        'install_puppet::install_gpg_key', $master, name => $os_family, gpg_url => $gpg_url
-      )
 
-      $major_version = $master_facts.first.value['os']['release']['major']
     }
     default: { fail_plan("Unsupported platform ${os_family}") }
   }
 
+  $gpg_result = run_task(
+    'repo_tasks::install_gpg_key', $master, name => $os_family, gpg_url => $gpg_url
+  )
+
   $repo_result = run_task(
-    'install_puppet::install_repo', $master, platform => $os_family, repo_url => $repo_url, name => 'puppet', extra_args => $extra_args
+    'repo_tasks::install_repo', $master, $os_family, repo_url => $repo_url, name => 'puppet'
   )
 
   $package_out = ['puppetserver', 'puppet-agent'].map |$p| {
@@ -60,23 +57,45 @@ plan install_puppet::provision_master(
   if $install_puppetdb {
     $services = ['puppetserver', 'puppetdb', 'puppet']
 
-    run_command('/opt/puppetlabs/bin/puppet module install puppetlabs-puppetdb', $master, _run_as => 'root')
+    run_command('/opt/puppetlabs/bin/puppet module install puppetlabs-puppetdb', $master, _run_as                    => 'root')
 
+    run_command('/opt/puppetlabs/bin/puppet apply -e "class{ \'puppetdb\':}"', $master, _run_as                             => 'root')
+    run_command('/opt/puppetlabs/bin/puppet apply -e "class{ \'puppetdb::master::config\':}"', $master, _run_as                             => 'root')
+
+  run_task('puppet_conf', $master, action => set, section => master, setting => reports, value => 'puppetdb')
+  run_task('puppet_conf', $master, action => set, section => agent, setting => report, value => 'true')
+    $master.apply_prep
     apply($master) {
-      file {'/etc/puppetlabs/code/environments/production/manifests/site.pp':
+      $user = "${facts['identity']['user']}"
+      case $user {
+        'root': { $home = '/root' }
+        default: { $home = "/home/$user" }
+      }
+
+      ["$home/.puppetlabs", "$home/.puppetlabs/client-tools"].each |$f| {
+        file {"$f":
+          ensure => directory,
+        }
+      }
+
+      file {"$home/.puppetlabs/client-tools/puppetdb.conf":
         ensure  => present,
-        content => epp('install_puppet/site.pp.epp', { 'hostname' => $master } )
+        content => epp('install_puppet/puppetdb.pp.epp', { 'hostname' => $master } ),
       }
     }
 
-    run_task('install_puppet::run_agent', $master)
+    run_task('run_agent::run_agent', $master)
+    run_command('/opt/puppetlabs/puppet/bin/gem install --bindir /opt/puppetlabs/bin puppetdb_cli', $master, _run_as => 'root')
+
+    # Kick puppetserver to pick up the report changes in puppet.conf
+    run_task('service::linux', $master, action => 'restart', name => 'puppetserver')
   }
   else {
     $services = ['puppetserver', 'puppet']
   }
 
   $service_out = $services.map |$s| {
-    $out = run_task('service', $master, action => 'status', name => $s).first
+    $out = run_task('service::linux', $master, action => 'status', name => $s).first
     $tmp = { $s => $out.value }
     $tmp
   }
